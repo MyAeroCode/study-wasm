0269e56556ff98064c94280e94ff545688de4c6e
AeroCode <AeroCode@localhost.localdomain>
2020-03-12 21:03:02 +0900

    edit: simple wasm-c playground

--- c/deps/wasm-loader.js
+++ c/deps/wasm-loader.js
@@ -1,4 +1,10 @@
 //
+// Pair of [idx, filename];
+const map = {
+    '01': 'adder',
+}
+
+//
 // WASM Env holder.
 let importObject = {
     env: {
@@ -16,19 +22,39 @@ let wasm = undefined;
 
 //
 // Load WASM using given module name.
-async function loadWASM(filename) {
+async function loadWASM() {
     try {
-        let buffer = await fetch(filename).then(res => res.arrayBuffer());
+        //
+        // get target idx param.
+        let url_string = document.location.href;
+        let url = new URL(url_string);
+        let idx = url.searchParams.get("idx");
+        if (idx === null) throw new Error(`url param[idx] is required.`);
+        if (map[idx] === undefined) throw new Error(`invaild idx : ${idx}`);
+
+        //
+        // get target url.
+        let target_folder = `${idx}-${map[idx]}`;
+        let target_module = `${map[idx]}.wasm`;
+        let target_url = `${target_folder}/${target_module}`;
+
+        //
+        // load target module.
+        let buffer = await fetch(target_url).then(res => res.arrayBuffer());
         let module = await WebAssembly.compile(buffer);
         let instance = new WebAssembly.Instance(module, importObject);
         wasm = instance.exports;
-        message("The module has been loaded successfully. Please check the debug console.");
 
+        //
+        // success.
+        message("The module has been loaded successfully. Please check the debug console.");
         console.log("The module has been loaded successfully.");
         console.log("Check the 'wasm' variable.");
     }
-    catch (_) {
-        message("Failed to load module.");
+    catch (e) {
+        //
+        // fail.
+        message(e.message);
     }
 
 };
